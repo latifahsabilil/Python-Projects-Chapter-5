6d8adc118ed9f676ac4e457bfd7c8b3dac52b5d6
latifahsabilil <71626855+latifahsabilil@users.noreply.github.com>
2020-10-28 00:46:19 +0700

    Create latihan 2_3.py

---
+++ LATIHAN 2/latihan 2_3.py
@@ -0,0 +1,14 @@
+#Latihan2
+#3
+
+sum = 0
+i = 1
+n = 0
+while (i <= 100):
+    print(i)
+    n = n + i
+    i = i + 2
+    sum = sum + 1
+print ('Banyaknya bilangan ganjil adalah ' + str(sum))
+print ('Jumlah seluruh bilangan adalah ' + str(n))
+
